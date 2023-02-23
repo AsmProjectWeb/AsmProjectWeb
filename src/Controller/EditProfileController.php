33b6ad12702438afa0d6c24940e0f5568c862b65
KhoaHN612 <khoahnvithuy@gmail.com>
2023-02-23 15:27:15 +0700

    Update Avartar and Show avartar, add header to profile page

--- src/Controller/EditProfileController.php
+++ src/Controller/EditProfileController.php
@@ -29,10 +29,9 @@ class EditProfileController extends AbstractController
      */
     public function index(Request $req,UserPasswordHasherInterface $userPasswordHasher, SluggerInterface $slugger,Security $security): Response
     {
-        //$user = null;
+        $user = null;
         $user = $this->security->getUser();
         $form = $this->createForm(ProfileType::class, $user);
-
 
         $form->handleRequest($req);
         if($form->isSubmitted() && $form->isValid()){
@@ -47,11 +46,11 @@ class EditProfileController extends AbstractController
             }
             $imgFile = $form->get('image')->getData();
             if ($imgFile) {
-                //$newFilename = $this->uploadAvatar($imgFile,$slugger);
-                //$user->setAvatar($newFilename);
+                $newFilename = $this->uploadAvatar($imgFile,$slugger);
+                $user->setAvatar($newFilename);
             }
             $this->repo->add($user,true);
-            return $this->redirectToRoute('profile', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('page', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('edit_profile/index.html.twig', [
@@ -59,7 +58,7 @@ class EditProfileController extends AbstractController
         ]);
     }
     public function uploadAvatar($imgFile, SluggerInterface $slugger): ?string{
-        $originalFilename = $this->getUser()->getUserIdentifier();
+        $originalFilename = $this->security->getUser()->getUserName();
         $safeFilename = $slugger->slug($originalFilename);
         $newFilename = $safeFilename.'-'.uniqid().'.'.$imgFile->guessExtension();
         try {
