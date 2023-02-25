c001631a9487a8a4f2c651310af3f566e57f5c0e
KhoaHN612 <khoahnvithuy@gmail.com>
2023-02-25 23:21:08 +0700

    Finished chat

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -2,12 +2,16 @@
 
 namespace App\Controller;
 
+use App\Repository\MessRepository;
 use App\Repository\UserRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+date_default_timezone_set('Asia/Ho_Chi_Minh');
 
 /**
  * @Route("/message")
@@ -31,10 +35,63 @@ class MessageController extends AbstractController
 
     }
     /**
-     * @Route("/chat", name="app_message")
+     * @Route("/chat/{to<\d+>}", name="app_message")
      */
-    public function chat(): Response
+    public function chat(int $to, Request $req, UserRepository $userRepo, MessRepository $messRepo): Response
     {
-        return $this->render('message/chat.html.twig');
+        $user = $userRepo->find($to);
+
+        if ($req->isXmlHttpRequest()) {
+            $curAcc = $this->getUser();
+            $from = $curAcc->getid();
+
+            $conId = $messRepo -> getIdConversation($from,$to);
+
+            $message = $req->request->get('message');
+            $isNew = $req->request->get('new');
+            if (!empty($message)){
+
+                $now = new DateTime();
+                $currentDatetime = $now->format('Y-m-d H:i:s');
+                $messObj = $messRepo-> sendMessPerson($conId,$from,$message,$currentDatetime);
+                
+                $messs = [];
+                $messs[] = [
+                    'id'=>$messObj->getId(),
+                    'userid'=>$messObj->getUser()->getId(),
+                    'content'=>$messObj->getContent(),
+                    'createAt'=>$messObj->getCreateAt()->format('Y-m-d H:i:s'),
+                ];
+
+                Return new JsonResponse($messs);  
+            } elseif ($isNew=='yes') {
+                $messObj = $messRepo->findBy(['conversation'=>$conId]);
+                $messs = [];
+                foreach ($messObj as $mess) {
+                    $messs[] = [
+                        'id'=>$mess->getId(),
+                        'userid'=>$mess->getUser()->getId(),
+                        'content'=>$mess->getContent(),
+                        'createAt'=>$mess->getCreateAt()->format('Y-m-d H:i:s'),
+                    ];
+                }
+                
+                return new JsonResponse($messs);
+                // Return new JsonResponse($messObj);
+            } else {
+                $messObj = $messRepo->findOneBy(['conversation'=>$conId],['createAt' => 'DESC']);
+                $messs = [];
+                $messs[] = [
+                    'id'=>$messObj->getId(),
+                    'userid'=>$messObj->getUser()->getId(),
+                    'content'=>$messObj->getContent(),
+                    'createAt'=>$messObj->getCreateAt()->format('Y-m-d H:i:s'),
+                ];
+                Return new JsonResponse(($messs));
+            }
+        }
+        return $this->render('message/chat.html.twig',[
+            'user'=>$user
+        ]);
     }
 }
--- src/Repository/MessRepository.php
+++ src/Repository/MessRepository.php
@@ -39,6 +39,50 @@ class MessRepository extends ServiceEntityRepository
         }
     }
 
+    public function getIdConversation($from,$to): int {
+        $conn = $this->getEntityManager()->getConnection();
+        $sql = "       
+            SELECT u1.id, u1.username, p1.conversation_id as conv, u2.id, u2.username, COUNT(*)
+            FROM 
+                (user u1 INNER JOIN participant p1 ON u1.id = p1.iduser_id
+                INNER JOIN conversation c1 ON p1.conversation_id = c1.id) INNER JOIN
+                (user u2 INNER JOIN participant p2 ON u2.id = p2.iduser_id
+                INNER JOIN conversation c2 ON p2.conversation_id = c2.id) ON p1.conversation_id = p2.conversation_id
+            WHERE u1.id < u2.id  
+            GROUP BY p1.conversation_id
+            HAVING COUNT(*)=1 AND u1.id IN (:from, :to) AND u2.id IN (:from, :to)
+        ";
+        $re = $conn->executeQuery($sql,['from'=>$from,'to'=>$to]);
+        if ($re->rowCount() == 0){
+            $sql2 = "INSERT INTO `conversation` (`id`, `last_message_id`) VALUES (NULL, NULL)";
+            $conn->executeQuery($sql2);
+            $sql3 = "SELECT id as id FROM conversation ORDER BY id DESC LIMIT 1";
+            $resql3 = $conn->executeQuery($sql3);
+            $con = $resql3->fetchAllAssociative();
+            $conId = intval($con[0]['id']);
+
+            $sql4 = "INSERT INTO `participant`(`message_read_at`, `iduser_id`, `conversation_id`) VALUES (NOW(), :from, :conid)";
+            $conn->executeQuery($sql4,['from'=>$from,'conid'=>$conId]);
+
+            $sql4 = "INSERT INTO `participant`(`message_read_at`, `iduser_id`, `conversation_id`) VALUES (NOW(), :to, :conid)";
+            $conn->executeQuery($sql4,['to'=>$to,'conid'=>$conId]);     
+        } else {
+            $conv = $re->fetchAllAssociative();
+            $conId = intval($conv[0]['conv']);
+        }
+        return $conId;
+    }
+
+    public function sendMessPerson($conId,$from,$mess,$time): Mess
+    {
+        $conn = $this->getEntityManager()->getConnection();
+        $sql5 = "INSERT INTO `mess`( `content`, `create_at`, `conversation_id`, `user_id`) VALUES (:mess, NOW(), :conId, :from)";
+        $conn->executeQuery($sql5,['mess'=>$mess,'conId'=>$conId,'from'=>$from]);
+
+        $mess = $this->findOneBy([],['createAt' => 'DESC']);
+        return $mess;
+    }
+
 //    /**
 //     * @return Mess[] Returns an array of Mess objects
 //     */
